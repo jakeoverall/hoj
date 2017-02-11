cf41d3c3292608de0f58a7fb9f2c4eeb6ef1470a
justinandrewfien <jfien12@live.com>
2017-02-11 05:04:26 -0700

    Cleaned Category Comp

--- public/app/components/category-component.js
+++ public/app/components/category-component.js
@@ -15,39 +15,23 @@ Vue.component('category', {
   },
   template: `
     <div class="container">
-    <br><br>
-        <h1>HELLO WORLS</hr>
-       <div class="row">
-            <div class="col-xs-12">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div v-for="question in questions">
-                            <router-link :to="'/questions/'+question._id">{{question.title}}</router-link>
-                    </div>
+      <h1>{{category.name}}</hr>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <div v-for="question in questions">
+                <router-link :to="'/questions/'+question._id">{{question.title}}</router-link>
+              </div>
             </div>
-       </div>
+          </div>
         </div>
-       </div>
+      </div>
     </div>
   `,
   methods: {
-    //get info from 
     categoryById: (categoryId) => { this.$root.$data.store.actions.getCategory(categoryId) },
-    addCategories: (category) => this.categories.push(category),
-    // vote:(voteValue, userId) => this.categories[i].push(category),
-    recentCategories: (categories) => categories.sort(),
-    sortNewCategories: (categories) => categories.sort((a, b) => {
-      if (path === true) {
-        return banana.created - apple.created
-      }
-    }),
-    sortAnsweredCategories: (categories) => categories.sort((a, b) => {
-      if (path === false) {
-        return banana.created - apple.created
-      }
-    }),
-
-
+   
   }
 
 
