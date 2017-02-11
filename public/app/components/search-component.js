eaf3b5d939a715e106d7d46dc26343edee76d8ff
dave limber <dave.limber@gmail.com>
2017-02-10 20:44:24 -0700

    updated search - need to clean

--- public/app/components/search-component.js
+++ public/app/components/search-component.js
@@ -1,14 +1,30 @@
-new Vue({
-  el: '#search',
-  data: {
-    searchString: '',
-    dummypath: new DummyStore,
-    ans_results: [],
-    
-
+Vue.component('search', {
+  data: function () {
+    return {
+      questions: [],
+    }
+  },
+  mounted: function() {
+    this.$root.$data.store.actions.getQuestions().then(response => {
+      this.questions = response.data.data
+    })
   },
 
-  methods: {
+    template: `
+    <div class="container">
+    <div class="row">
+    <ul>
+               
+        <li v-for="title in filteredAnswers">
+                <p>{{filteredAnswers}}</p>
+        </li>
+    </ul>
+      
+    </div>
+    </div>
+  `,
+
+    methods: {
 
   },
 
@@ -16,25 +32,23 @@ new Vue({
   
         filteredAnswers: function () {
             
-            //STORAGE INFO NEEDED
-            var ans_array = this.STORAGE.INFO;
+            var ans_array = this.questions;
             
-                // GOOD
                 searchString = this.searchString;
 
             if(!searchString){
+console.log ('not search string')
                 return ans_array;
             }
 
             searchString = searchString.trim().toLowerCase();
-
+console.log('after not search string')
             ans_array = ans_array.filter(function(item){
                 if(item.title.toLowerCase().indexOf(searchString) !== -1){
                     return item;
                 }
             })
-
-        
+   
             return ans_array;
         }
     },
