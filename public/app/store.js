11127881ded85de78c7a73545b98436eb9ccf0b3
dsottimano <dsottimano@gmail.com>
2017-02-10 19:59:38 -0700

    store gets functional

--- public/app/store.js
+++ public/app/store.js
@@ -6,8 +6,27 @@ function Store(){
     user: {}
   }
   this.actions = {
+    getCategories : function () {
+     return Vue.http.get('/categories')
+    },
+    getCategory : function (categoryId) {
+      return Vue.http.get('/categories/' + categoryId)
+    },
     getQuestions: function(){
       return Vue.http.get('/questions')
+    },
+    getQuestion: function(questionId){
+      return Vue.http.get('/questions/' + questionId)
+    },
+    getComments: function () {
+      return Vue.http.get('/comments')
+    },
+    getComment: function (commentId) {
+      return Vue.http.get('/comments/' + commentId)
+    },
+    postCategories : function (category) {
+      return Vue.http.post('/categories',category)
     }
+
   }
 }
