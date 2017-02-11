4782585a342f79876e7783be5d147ed332c82a83
Jake Overall <joverall22@gmail.com>
2017-02-10 18:25:11 -0700

    getting questions

--- public/app/components/questions-component.js
+++ public/app/components/questions-component.js
@@ -11,33 +11,40 @@
 //   `
 // })
 
-new Vue({
-  el: '#questions',
-  data:{
-    questions: [],
-    dummypath: new DummyStore
-    
-
+Vue.component('questions', {
+  data: function () {
+    return {
+      questions: [],
+    }
   },
-  mounted(){
-    this.questions = dummyservice.getQuestions()
-    this.sortNewQuestions(this.questions)
+  mounted() {
+    this.$root.$data.store.actions.getQuestions().then(response => {
+      this.questions = response.data.data
+    })
+    // this.sortNewQuestions(this.questions)
   },
-  methods:{
+  template: `
+    <div>
+      {{questions}}
+    </div>
+  `,
+  methods: {
     //get info from 
-    questionById: (questionId) => {dummypath.getquestion(questionId)},
-    addQuestion:(question) => this.questions.push(question),
+    questionById: (questionId) => { dummypath.getquestion(questionId) },
+    addQuestion: (question) => this.questions.push(question),
     // vote:(voteValue, userId) => this.questions[i].push(question),
     recentQuestions: (questions) => questions.sort(),
-    sortNewQuestions: (questions) => questions.sort((a,b) =>{
-      if(path === true){
-      return banana.created - apple.created}
+    sortNewQuestions: (questions) => questions.sort((a, b) => {
+      if (path === true) {
+        return banana.created - apple.created
+      }
     }),
-    sortAnsweredQuestions: (questions) => questions.sort((a,b) =>{
-      if(path === false){
-      return banana.created - apple.created}
+    sortAnsweredQuestions: (questions) => questions.sort((a, b) => {
+      if (path === false) {
+        return banana.created - apple.created
+      }
     }),
-    
+
 
   }
 
